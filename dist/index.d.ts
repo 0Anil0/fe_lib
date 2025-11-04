31f70d28156bf9c3e8201d1dd9cbf102d67527eb
Anil Sharma <anil.sharma@webanix.in>
2025-11-04 12:59:30 +0530

    Chnage in vite

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+export {}
+import Fe_lib_dev_anil from './src/index'
+export default Fe_lib_dev_anil
+export {}
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import dts from 'vite-plugin-dts';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [react(), dts({ insertTypesEntry: true })],
   build: {
     lib: {
-      entry: 'src/index.ts',
-      name: 'fe_lib_dev_anil',
+      entry: "src/index.ts",
+      name: "fe_lib_dev_anil",
       fileName: (format) => `fe-lib-dev-anil.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'antd'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          antd: 'antd',
-        },
-      },
+      external: ["react", "react-dom", "antd"],
     },
   },
 });
